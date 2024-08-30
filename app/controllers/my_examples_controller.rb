b236de217f6d4cea2b679c0b32ff9921ef1056e1
Leilanigreer <leilani@lpcgolf.com>
2024-08-29 17:15:55 -0700

    added random fortune route

--- app/controllers/my_examples_controller.rb
+++ app/controllers/my_examples_controller.rb
@@ -1,2 +1,17 @@
 class MyExamplesController < ApplicationController
+
+  def fortune_picker(var1, var2, var3)
+    [var1, var2, var3].sample
+  end
+
+  def random_fortune
+    fortune1 = "going camping with your least favorite person"
+    fortune2 = "owning a billion dollars but not being to spend it"
+    fortune3 = "going on the vaction you always dreamed of"
+
+    render json: "Your long awaited dream of #{fortune_picker(fortune1, fortune2, fortune3)} is coming true!"
+  end
+
+  def lotto_numbers
+
 end
